decd1ad40d356473b40b901a0a1f411e39dd03b8
Mykola Paziuk <external.Mykola.Paziuk@se.bosch.com>
2024-01-08 18:34:52 +0200

    Added listener and toast with message

--- app/src/main/java/com/example/githelloworld_lh10/MainActivity.kt
+++ app/src/main/java/com/example/githelloworld_lh10/MainActivity.kt
@@ -1,4 +1,21 @@
 package com.example.githelloworld_lh10
 
-class MainActivity {
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity:Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        val button:Button = findViewById(R.id.myButton)
+        button.setOnClickListener {
+            val toast = Toast.makeText(this, "Button pressed!", Toast.LENGTH_LONG)
+            toast.show()
+        }
+
+    }
+
 }

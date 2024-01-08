a34aaa2e49f8217ae066ec1f45cea252151d11f7
Mykola Paziuk <external.Mykola.Paziuk@se.bosch.com>
2024-01-08 17:46:11 +0200

    Start commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GitHelloWorld_lh10"
+include(":app")
+ 
